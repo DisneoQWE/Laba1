dc03d18af20694af696fdb295a2d7cbfe2e3c0b2
Sagindykov Arsan <disneoqwe@gmail.com>
2021-05-06 00:22:52 +0700

    Registry class is modified 26.04.21

--- registry.h
+++ registry.h
@@ -1,15 +1,24 @@
 #ifndef REGISTRY_H
 #define REGISTRY_H
 #include <QList>
+#include"company.h"
 /*
- * Класс reqistry представляет собой класс синглтон
-*/
+ * Реестр предприятий представляет собой класс
+ * Singleton, содержащий список (QList) предприятий
+ * и методы, необходимые для работы со списком:*/
 class registry{
 private:
-    QList
+    QList<company* > companyList;
+    registry(const QList<company* >& cl) :
+        companyList(cl) {}
+    registry() {}
 public:
-    registry();
-
+    registry(registry& re) = delete;
+    void operator=(const registry& re) = delete;
+    static registry& getInstance();
+    void AddCompany(company& c);
+    company* getCompanyByIndex(int index) const;
+    int getRegistrySize() const;
 };
 
 #endif // REGISTRY_H
